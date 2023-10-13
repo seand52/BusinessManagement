cfd04caeb5376c5dd21ad49ea6ed5ae636a716dd
Sean <seand52@gmail.com>
2023-10-13 16:35:55 +0200

    Grant permissions to user my my user for database

---
+++ Migrations/20231013142642_GrantPermissions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class GrantPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // grant permissions to all tables in database dev_business_management to user seand in postgres
+            migrationBuilder.Sql(@"GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO seand;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public FROM seand;");
+        }
+    }
+}
